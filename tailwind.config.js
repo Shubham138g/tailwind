1126b3edfd26eee558fa0c9833f1d930292e5392
shubham <tonygupta275@gmail.com>
2024-06-21 20:26:23 +0530

    add hover and main & nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-orange': '#FF5F00', // Define your custom color here
+        'custom-blue': '#002379',
+        'custome-black':'#000000', // Define another custom color
+        'custome-pink':'#F72798', // Define another custom color
+        // Add more custom colors as needed
+      },
+    },
   },
   plugins: [],
 }
